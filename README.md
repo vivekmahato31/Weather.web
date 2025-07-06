e4b02f9acb2d31e648103f694f91e1c801f3a861
vivekmahato31 <vivekmahato31@users.noreply.github.com>
2025-07-06 13:27:49 +0530

    final commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Weather.web
+A simple, responsive weather website built with HTML, CSS, and JavaScript. Users can search any city to get real-time weather details like temperature, condition, humidity, and wind speed.
---
+++ script.js
@@ -0,0 +1,54 @@
+const apiKey = "730434b37d0227b78d93b3c43194acaa";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon img");
+const weatherVideo = document.getElementById("weatherVideo");
+
+const videos = {
+  clear: "assets/videos/clear.mp4",
+  clouds: "assets/videos/clouds.mp4",
+  drizzle: "assets/videos/Drizzle.mp4",
+  mist: "assets/videos/mist.mp4",
+  rain: "assets/videos/rain.mp4",
+  snow: "assets/videos/snow.mp4",
+  thunderstorm: "assets/videos/Thunderstorm.mp4"
+};
+
+function getCondition(main) {
+  main = main.toLowerCase();
+  if (main.includes("cloud")) return "clouds";
+  if (main.includes("rain")) return "rain";
+  if (main.includes("snow")) return "snow";
+  if (main.includes("thunderstorm")) return "thunderstorm";
+  if (main.includes("drizzle")) return "drizzle";
+  if (main.includes("mist") || main.includes("haze") || main.includes("fog")) return "mist";
+  return "clear";
+}
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  if (response.status == 404) {
+    alert("Invalid City Name");
+  } else {
+    const data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    document.querySelector(".weather-desc").innerHTML = data.weather[0].main;
+
+    const condition = getCondition(data.weather[0].main);
+    weatherIcon.src = `assets/images/${condition}.png`;
+
+    
+    weatherVideo.src = videos[condition];
+    weatherVideo.play();
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
