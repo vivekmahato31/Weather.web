8c4c9d1fb790443035bc48ed1e947865d3da56a2
vivekmahato31 <vivekmahato31@users.noreply.github.com>
2025-07-06 14:13:55 +0530

    Add event listener for Enter key in search box to trigger weather check

--- script.js
+++ script.js
@@ -51,3 +51,9 @@ async function checkWeather(city) {
 searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
 });
+
+searchBox.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    checkWeather(searchBox.value);
+  }
+});
